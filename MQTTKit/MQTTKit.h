5d916d1d9b602e5f41be7d2aec3e9128cd53826b
arnecs <arnecs@arnechristian.local>
2018-03-28 15:12:46 +0200

    Initial Commit

---
+++ MQTTKit/MQTTKit.h
@@ -0,0 +1,19 @@
+//
+//  MQTTKit.h
+//  MQTTKit
+//
+//  Created by Arne Christian Skarpnes on 28.03.2018.
+//  Copyright © 2018 Arne Christian Skarpnes. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MQTTKit.
+FOUNDATION_EXPORT double MQTTKitVersionNumber;
+
+//! Project version string for MQTTKit.
+FOUNDATION_EXPORT const unsigned char MQTTKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MQTTKit/PublicHeader.h>
+
+
